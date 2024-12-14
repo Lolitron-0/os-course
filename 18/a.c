18fbfe32b8aac9b5fbbf4b193c5902c978966053
Lolitron-0 <ovcharov_05@mail.ru>
2024-12-14 10:14:49 +0300

    fix

--- 18/a.c
+++ 18/a.c
@@ -7,31 +7,39 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+uint32_t number;
+uint32_t mask = 1U << 31;
+pid_t other_pid;
+
+void my_handler(int nsig) {
+  if (number & mask) {
+    if (kill(other_pid, SIGUSR1) < 0) {
+      perror("can't kill");
+      exit(1);
+    }
+  } else {
+    if (kill(other_pid, SIGUSR2) < 0) {
+      perror("can't kill");
+      exit(1);
+    }
+  }
+  mask >>= 1;
+}
+
 int main() {
   pid_t pid = getpid();
-  pid_t other_pid;
-  uint32_t number;
   printf("my pid: %d\n", pid);
   printf("other pid: ");
   scanf("%d", &other_pid);
   printf("number: ");
   scanf("%u", &number);
 
-  uint32_t mask = 1U << 31;
-  for (int i = 0; i < 32; i++) {
-    if (number & mask) {
-      if (kill(other_pid, SIGUSR1) < 0) {
-        perror("can't kill");
-        exit(1);
-      }
-    } else {
-      if (kill(other_pid, SIGUSR2) < 0) {
-        perror("can't kill");
-        exit(1);
-      }
-    }
-    mask >>= 1;
-    usleep(1000);
+  signal(SIGUSR1, my_handler);
+
+  my_handler(SIGUSR1);
+
+  while (mask) {
+    sleep(1);
   }
 
   return 0;
--- 18/b.c
+++ 18/b.c
@@ -11,6 +11,7 @@
 uint32_t number = 0;
 int recieved_bits = 0;
 bool done = false;
+pid_t other_pid;
 
 void my_handler(int nsig) {
   number <<= 1;
@@ -23,11 +24,18 @@ void my_handler(int nsig) {
   if (recieved_bits == 32) {
     done = true;
   }
+
+  if (kill(other_pid, SIGUSR1) < 0) {
+    perror("can't kill");
+    exit(1);
+  }
 }
 
 int main() {
   pid_t pid = getpid();
   printf("my pid: %d\n", pid);
+  printf("other pid: ");
+  scanf("%d", &other_pid);
 
   signal(SIGUSR1, my_handler);
   signal(SIGUSR2, my_handler);
